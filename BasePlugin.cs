f4c4736b2d9d726dcda09bcb90d2396fa2d9b82d
PixelGuy123 <89644121+PixelGuy123@users.noreply.github.com>
2024-01-12 15:40:32 -0300

    Adicionar arquivos de projeto.

---
+++ BasePlugin.cs
@@ -0,0 +1,35 @@
+﻿using BepInEx;
+using HarmonyLib;
+using MTM101BaldAPI.Registers;
+
+namespace BBSchoolMaze.Plugin
+{
+	[BepInPlugin(ModInfo.GUID, ModInfo.Name, ModInfo.Version)]
+	[BepInDependency("pixelguy.pixelmodding.baldiplus.bbgenfixes", BepInDependency.DependencyFlags.HardDependency)] // Requires elevator fix
+
+	public class BasePlugin : BaseUnityPlugin
+	{
+		void Awake()
+		{
+			GeneratorManagement.Register(this, GenerationModType.Override, (_, _2, ld) =>
+			{
+				ld.timeBonusLimit *= 2;
+				ld.timeBonusVal *= 2;
+			});
+
+			Harmony harmony = new(ModInfo.GUID);
+			harmony.PatchAll();
+		}
+
+	}
+
+
+	internal static class ModInfo
+	{
+		internal const string GUID = "pixelguy.pixelmodding.baldiplus.bbcrazymaze";
+		internal const string Name = "BB+ Crazy School Maze";
+		internal const string Version = "1.0.0";
+	}
+
+
+}
---
+++ ModPatches.cs
@@ -0,0 +1,131 @@
+﻿using HarmonyLib;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using TMPro;
+using UnityEngine;
+
+namespace BBSchoolMaze.Patches
+{
+
+	[HarmonyPatch(typeof(LevelGenerator))]
+	internal class MazeChaos
+	{
+
+		[HarmonyPatch("StartGenerate")]
+		[HarmonyPrefix]
+		private static void GetGenerator(LevelGenerator __instance)
+		{
+			tripEntrances.Clear();
+			i = __instance;
+		}
+		
+
+		[HarmonyPatch("Generate", MethodType.Enumerator)]
+		[HarmonyTranspiler]
+		private static IEnumerable<CodeInstruction> EstablishChaos(IEnumerable<CodeInstruction> instructions)
+		{
+			var match = new CodeMatcher(instructions)
+			.MatchForward(false,
+				new CodeMatch(OpCodes.Ldnull),
+				new CodeMatch(OpCodes.Ldc_I4_1),
+				new CodeMatch(OpCodes.Ldc_I4_0),
+				new CodeMatch(OpCodes.Call, AccessTools.Method(typeof(LevelBuilder), "AddRandomDoor")) // Lesson learned: one code match doesn't work, you have to be super specific
+				); // Get to this method
+
+			match.Advance(-7);
+			match.InsertAndAdvance(
+			Transpilers.EmitDelegate(() =>
+			{
+				foreach (var tile in i.Ec.mainHall.GetNewTileList())
+				{
+					if (!tile.keepBin)
+					{
+						i.Ec.DestroyTile(tile.position, tile);
+						i.Ec.mainHall.RemoveTile(tile);
+					}
+				}
+
+				var maze = new GameObject("MazeGen").AddComponent<MazeGenerator>();
+				i.Ec.mainHall.size = i.levelSize;
+				i.Ec.mainHall.maxSize = i.levelSize;
+
+				AccessTools.Field(typeof(MazeGenerator), "patchesToSpawn").SetValue(maze, 0); // No "library" centers
+
+				// Setups the maze manually to call the Generate() method
+				AccessTools.Field(typeof(MazeGenerator), "cRng").SetValue(maze, new System.Random(i.controlledRNG.Next()));
+
+				var method = (IEnumerator)AccessTools.Method("MazeGenerator:Generate", [typeof(RoomController)]).Invoke(maze, [i.Ec.mainHall]); // Invokes the method, now just manually wait
+				while (method.MoveNext()) { } // Manually move next the method
+
+				foreach (var door in tripEntrances)
+				{
+					var tile = i.Ec.TileFromPos(door.Key);
+					if (tile != null)
+					{
+						tile.doorHere = true;
+						tile.doorDirs.Add(door.Value);
+						tile.doorDirsSpace.Add(door.Value); // Fix the issue with blocked off entrances
+					}
+				}
+
+				tripEntrances.Clear(); // Done
+			}));
+
+			return match.InstructionEnumeration();
+		}
+
+		static LevelGenerator i;
+
+		internal static Dictionary<IntVector2, Direction> tripEntrances = [];
+	}
+
+
+	[HarmonyPatch(typeof(BaseGameManager), "Initialize")]
+	internal class NocheatJustfullmap
+	{
+		private static void Prefix(BaseGameManager __instance) =>
+			__instance.CompleteMapOnReady();
+		
+	}
+
+	[HarmonyPatch(typeof(StoreScreen), "Start")]
+	internal class NoFullmapForYou
+	{
+		private static void Postfix(ref TMP_Text ___mapPriceText, ref bool[] ___itemPurchased, ref GameObject ___mapHotSpot)
+		{
+			___itemPurchased[6] = true; // It is the fullmap index
+			___mapPriceText.text = "Out";
+			___mapPriceText.color = Color.red; // Map price text, duh
+			___mapHotSpot.SetActive(false); // That hover spot for johnny to explain the item
+		}
+	}
+
+	[HarmonyPatch(typeof(LevelBuilder))]
+	internal class MakeElevatorVisible
+	{
+		[HarmonyPatch("Start")]
+		[HarmonyPrefix]
+		private static void ChangeConstBins(ref MapTile[] ___mapTiles) =>
+			___mapTiles[16] = ___mapTiles[15]; // Literally this
+
+		[HarmonyPatch("AddMapTile")]
+		[HarmonyPostfix]
+		private static void FixElevatorColor(ref IntVector2 position, EnvironmentController ___ec, ref Map ___map) // Basically make elevators cyan
+		{
+			TileController tileController = ___ec.tiles[position.x, position.z];
+			if (tileController != null && tileController.ConstBin == 16)
+				___map.tiles[position.x, position.z].SpriteRenderer.color = Color.cyan;
+			
+		}
+
+		[HarmonyPatch("CreateTripEntrance")]
+		[HarmonyPostfix]
+		private static void RegisterThisEntrance(IntVector2 pos, Direction dir) =>
+			MazeChaos.tripEntrances.Add(pos, dir); // Register this entrance to fix a later bug
+		
+		
+	}
+
+
+}
